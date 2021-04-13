92a3cbe8bc9262d4f91256e54ae0554ef3795143
Wxffel <66750759+Wxffel@users.noreply.github.com>
2021-04-13 02:31:07 +0200

    Update PirateKit.java
    - added single explosions when leftclicked
    - lowered defaultExplosionPower and renamed it
    - added kitmessage keys (not done yet)
    - added back detonateExplosionBarrel method =)
    - fixed typos
    - fixed getAdditionalExplosionPower method
    - working on chainreactions
    - needs decision making about explosions via pressure plate
    
    (forgot to commit it one by one)

--- src/main/java/de/hglabor/plugins/kitapi/kit/kits/PirateKit.java
+++ src/main/java/de/hglabor/plugins/kitapi/kit/kits/PirateKit.java
@@ -9,25 +9,31 @@ import de.hglabor.plugins.kitapi.kit.items.KitItemBuilder;
 import de.hglabor.plugins.kitapi.kit.settings.FloatArg;
 import de.hglabor.plugins.kitapi.kit.settings.IntArg;
 import de.hglabor.plugins.kitapi.player.KitPlayer;
+import de.hglabor.plugins.kitapi.util.Logger;
+import de.hglabor.utils.localization.Localization;
 import de.hglabor.utils.noriskutils.ChatUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Barrel;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Egg;
 import org.bukkit.entity.Fireball;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.block.*;
+import org.bukkit.event.entity.ExplosionPrimeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.metadata.FixedMetadataValue;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static de.hglabor.utils.localization.Localization.t;
 
@@ -37,7 +43,7 @@ public class PirateKit extends MultipleKitItemsKit implements Listener {
     @IntArg
     private final int explosionBarrelsLimit, fireballSpeed;
     @FloatArg(min = 0.1F, max = 100f)
-    private final float explosionPower, additionalExplosionPowerStep, maxAdditionalExplosionPower;
+    private final float defaultExplosionPower, additionalExplosionPowerStep, maxAdditionalExplosionPower;
 
     private final String explosionBarrelMetaKey;
     private final String explosionBarrelsKey;
@@ -46,6 +52,7 @@ public class PirateKit extends MultipleKitItemsKit implements Listener {
     private final ItemStack canon = new KitItemBuilder(Material.FIRE_CHARGE).setName("Kanone").setDescription("Abschuss!!").build();
     private final ItemStack remoteDetonator = new KitItemBuilder(Material.TRIPWIRE_HOOK).setName("Fernzünder").setDescription("Explosion!!").build();
 
+    // todo: Barrels als additional kititem adden (3x)
     protected PirateKit() {
         super("Pirate", Material.FIRE_CHARGE);
         Map<KitItemAction, Float> kitActions = Map.of(
@@ -54,7 +61,7 @@ public class PirateKit extends MultipleKitItemsKit implements Listener {
         );
         setItemsAndCooldown(kitActions);
         explosionBarrelsLimit = 3;
-        explosionPower = 5F;
+        defaultExplosionPower = 3F;
         fireballSpeed = 2;
         explosionBarrelMetaKey = this.getName() + "explosionBarrel";
         explosionBarrelsKey = this.getName() + "explosionBarrelsList";
@@ -64,21 +71,30 @@ public class PirateKit extends MultipleKitItemsKit implements Listener {
 
     @KitEvent
     public void onPlayerLeftClicksOneOfMultipleKitItems(PlayerInteractEvent event, KitPlayer kitPlayer, ItemStack item) {
-        //TODO nacheinander
+        if (item.isSimilar(remoteDetonator)) {
+            List<Block> barrels = kitPlayer.getKitAttributeOrDefault(explosionBarrelsKey, Collections.emptyList());
+            if (!barrels.isEmpty()) {
+                successfulDetonated(Bukkit.getPlayer(kitPlayer.getUUID()), 1);
+                detonateExplosionBarrel(barrels.get(0));
+                barrels.remove(0);
+                kitPlayer.putKitAttribute(explosionBarrelsKey, barrels);
+            } else nothingChangedMsg(event.getPlayer());
+        }
     }
 
     @KitEvent
     public void onPlayerRightClicksOneOfMultipleKitItems(PlayerInteractEvent event, KitPlayer kitPlayer, ItemStack item) {
         Player player = event.getPlayer();
-        World world = player.getWorld();
-
         if (item.isSimilar(remoteDetonator)) {
             List<Block> barrels = kitPlayer.getKitAttributeOrDefault(explosionBarrelsKey, Collections.emptyList());
-            for (Block barrel : barrels) {
-                if (barrel.hasMetadata(explosionBarrelMetaKey)) {
-                    world.createExplosion(barrel.getLocation(), explosionPower);
-                }
+            if (barrels.isEmpty()) {
+                nothingChangedMsg(player);
+                return;
             }
+            for (Block barrel : barrels) {
+                detonateExplosionBarrel(barrel);
+            }
+            successfulDetonated(player, barrels.size());
             barrels.clear();
         } else if (item.isSimilar(canon)) {
             player.launchProjectile(Fireball.class, player.getEyeLocation().getDirection().multiply(fireballSpeed));
@@ -89,37 +105,46 @@ public class PirateKit extends MultipleKitItemsKit implements Listener {
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent event) {
         Block barrel = event.getBlockPlaced();
-        if (!barrel.getType().equals(Material.BARREL)) {
-            return;
-        }
+        if (barrel.getType() != Material.BARREL) return;
         Player player = event.getPlayer();
         KitPlayer kitPlayer = KitApi.getInstance().getPlayer(player);
         if (kitPlayer.hasKit(this)) {
             List<Block> barrels = kitPlayer.getKitAttributeOrDefault(explosionBarrelsKey, new ArrayList<>());
+
+            if (barrels.size() >= explosionBarrelsLimit) {
+                event.setCancelled(true); // produces error, nothing major
+                String key = "pirate.limitreached";
+                Locale locale = ChatUtils.getPlayerLocale(player);
+                ImmutableMap<String, String> arguments = ImmutableMap.of("limit", String.valueOf(explosionBarrelsLimit));
+                player.sendMessage(t(key, arguments, locale));
+                return;
+            }
+
             barrel.setMetadata(explosionBarrelMetaKey, new FixedMetadataValue(KitApi.getInstance().getPlugin(), ""));
-            barrel.setMetadata(UUID_KEY, new FixedMetadataValue(KitApi.getInstance().getPlugin(), player.getUniqueId()));
+            barrel.setMetadata(UUID_KEY, new FixedMetadataValue(KitApi.getInstance().getPlugin(), kitPlayer.getUUID()));
             barrels.add(barrel);
             kitPlayer.putKitAttribute(explosionBarrelsKey, barrels);
-            //TODO place message?
+
+            String key = "pirate.placesuccess";
+            Locale locale = ChatUtils.getPlayerLocale(player);
+            ImmutableMap<String, String> arguments = ImmutableMap.of("limit", String.valueOf(explosionBarrelsLimit));
+            player.sendMessage(t(key, arguments, locale));
+
         }
     }
 
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
         Block block = event.getBlock();
-        if (block.hasMetadata(explosionBarrelMetaKey) && block.hasMetadata(UUID_KEY)) {
+        if (isExplosionBarrel(block)) {
             UUID owner = (UUID) block.getMetadata(UUID_KEY).get(0).value();
-            if (owner == null) {
-                return;
-            }
+            if (owner == null) return;
             Optional<Player> optionalPlayer = Optional.ofNullable(Bukkit.getPlayer(owner));
             optionalPlayer.ifPresent(player -> {
-                String key = "pirat.destroyed";
+                String key = "pirate.destroyed";
                 Locale locale = ChatUtils.getPlayerLocale(owner);
                 ImmutableMap<String, String> arguments = ImmutableMap.of("location", block.getLocation().toString());
-
                 player.sendMessage(t(key, arguments, locale));
-
                 KitPlayer kitPlayer = KitApi.getInstance().getPlayer(player);
                 List<Block> barrels = kitPlayer.getKitAttributeOrDefault(explosionBarrelsKey, Collections.emptyList());
                 barrels.removeIf(barrel -> barrel.equals(block));
@@ -127,21 +152,88 @@ public class PirateKit extends MultipleKitItemsKit implements Listener {
         }
     }
 
-   /* private void detonateExplosionBarrel(World world, UUID playerUUID) {
-        Block explosionBarrel = world.getBlockAt(explosionBarrelsHolder.get(playerUUID).get(0));
-        if (explosionBarrel.hasMetadata(explosionBarrelMetaKey)) {
-            //whats the 100F?
-            float min = Math.min(Math.min(explosionPower + getAdditionalExplosionPower(explosionBarrel), maxAdditionalExplosionPower), 100f);
-            explosionBarrel.getWorld().createExplosion(explosionBarrel.getLocation(), min);
-            explosionBarrel.setType(Material.AIR);
-            explosionBarrelsHolder.get(playerUUID).remove(0);
+    // ist verantwortlich für kettenreaktionen =)
+    // todo: nur bei einer "linksklick zündung" abfragen, ansonsten explodieren sowieso alle
+    // maybe kleinen delay adden, damit es realistischer ist (:
+    @EventHandler
+    public void onExplosion(BlockExplodeEvent event) {
+
+        List<Block> explosionsBarrels = event.blockList().stream()
+                .filter(Objects::nonNull)
+                .filter(this::isExplosionBarrel)
+                .collect(Collectors.toList());
+
+        explosionsBarrels.forEach(block -> {
+            detonateExplosionBarrel(block);
+            KitPlayer kitPlayer = KitApi.getInstance().getPlayer(Bukkit.getPlayer((UUID) block.getMetadata(UUID_KEY).get(0).value())); // risky
+            List<Block> barrels = kitPlayer.getKitAttributeOrDefault(explosionBarrelsKey, Collections.emptyList());
+            barrels.removeIf(barrel -> barrel.equals(block));
+            kitPlayer.putKitAttribute(explosionBarrelsKey, barrels);
+        });
+
+        //event.getBlock().getWorld().getPlayers().get(0).sendMessage("explosionsBarrels: " + explosionsBarrels.size()); // debug
+    }
+
+    private void detonateExplosionBarrel(Block block) {
+        if (isExplosionBarrel(block)) {
+            float explosionPower = defaultExplosionPower + getAdditionalExplosionPower(block);
+            float limitedExplosionPower = Math.min(Math.min(explosionPower, maxAdditionalExplosionPower), 100f); // 100f = explosionPower (max)
+            block.setType(Material.AIR); // have to be done so the barrel cant be reused!
+            block.getWorld().createExplosion(block.getLocation(), limitedExplosionPower, true, true);
+            //block.getWorld().getPlayers().get(0).sendMessage("Ich habe ausgelöst."); // debug
         }
-    } */
+    }
 
     private float getAdditionalExplosionPower(Block block) {
-        AtomicInteger amount = new AtomicInteger();
-        Arrays.stream(((Barrel) block).getInventory().getStorageContents()).filter(itemStack -> itemStack.getType() == Material.GUNPOWDER).collect(Collectors.toList()).forEach(itemStack -> amount.addAndGet(itemStack.getAmount()));
-        return amount.get() * additionalExplosionPowerStep;
+        ItemStack[] invContents = ((Barrel) block.getState()).getInventory().getStorageContents();
+        if (invContents == null) return 0;
+        int amount = Arrays.stream(invContents)
+                .filter(Objects::nonNull)
+                .filter(itemStack -> itemStack.getType() == Material.GUNPOWDER)
+                .mapToInt(ItemStack::getAmount).sum();
+        return Math.min(amount * additionalExplosionPowerStep, maxAdditionalExplosionPower);
+    }
+
+    private boolean isExplosionBarrel(Block block) {
+        return block.getType() == Material.BARREL && block.hasMetadata(explosionBarrelMetaKey) && block.hasMetadata(UUID_KEY);
+    }
+
+    private void nothingChangedMsg(Player player) {
+        player.sendMessage(Localization.INSTANCE.getMessage("pirate.nothingChanged", ChatUtils.getPlayerLocale(player)));
+    }
+
+    private void successfulDetonated(Player player, Integer amount) {
+        String key = "pirate.successfulDetonated";
+        Locale locale = ChatUtils.getPlayerLocale(player);
+        ImmutableMap<String, String> arguments = ImmutableMap.of("amount", String.valueOf(amount));
+        player.sendMessage(t(key, arguments, locale));
     }
 }
 
+/* Passt alles, nur die pressure plate wird nicht remove
+ Halber Demoman - ist das überhaupt geil für Pirat?
+ Anyways.. erstmal nicht removen!!*/
+
+/*    // triggers when a barrel is directly powered from a pressure plate
+    @EventHandler
+    private void onTriggerExplosionBarrel(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        Block clickedBlock = event.getClickedBlock();
+        if (event.getAction() == Action.PHYSICAL && clickedBlock != null) {
+            if (clickedBlock.getType().toString().toLowerCase().endsWith("plate")) {
+                Block blockUnder = clickedBlock.getLocation().clone().subtract(0.0, 1.0, 0.0).getBlock();
+                if (isExplosionBarrel(blockUnder)) {
+                    KitPlayer kitPlayer = KitApi.getInstance().getPlayer(player);
+                    List<Block> barrels = kitPlayer.getKitAttribute(explosionBarrelsKey);
+
+                    barrels.remove(blockUnder);
+                    kitPlayer.putKitAttribute(explosionBarrelsKey, barrels);
+
+                    clickedBlock.setType(Material.AIR); // lol klappt auch nicht
+
+                    detonateExplosionBarrel(blockUnder);
+                    successfulDetonated(player, 1);
+                }
+            }
+        }
+    }*/
